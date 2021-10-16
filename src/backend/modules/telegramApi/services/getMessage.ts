303e38e172fae1015751c0337eb0df8382126cf7
Matheus Araújo <souzamatheus8174@gmail.com>
2021-10-16 14:43:05 -0300

    refactot: update getMessage

--- src/backend/modules/telegramApi/services/getMessage.ts
+++ src/backend/modules/telegramApi/services/getMessage.ts
@@ -1,11 +1,13 @@
 import { TelegramClient } from 'telegram';
 import { EntityLike } from 'telegram/define';
 
-function getMessage(client: TelegramClient, user?: EntityLike) {
-  return client.getMessages(user, {
-    limit: undefined,
+async function getMessage(client: TelegramClient, user?: EntityLike) {
+  const messageInfo = await client.getMessages(user, {
+    limit: 5,
     filter: undefined
   });
+
+  return messageInfo.map((element) => element.message);
 }
 
 export default getMessage;
